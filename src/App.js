d4fcddce72f84f38f170b56766399f5b55208675
George Fernandes <paodequeijooj@outlook.com>
2022-10-19 10:41:29 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from 'react';
+import TodoList from './TodoList';
+import uuid from 'react-uuid'
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  useEffect(()=> {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify)
+  }, [todos])
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    setTodos(prevTodos =>{
+      return [...prevTodos, { id: uuid(), name: name, complete: true}]
+    })
+    todoNameRef.current.value = null
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <>
+    <TodoList todos={todos}/>
+    <input ref={todoNameRef} type='text'/>
+    <button onClick={handleAddTodo}>add</button>
+    <button>clear</button>
+    <div>0 left</div>
+  </>
+  )
 }
 
 export default App;
